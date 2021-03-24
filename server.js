9122fc2135594b55394cc1c5bafe8f87fdf770fc
benjadahl <benjadahl@gmail.com>
2021-03-24 13:13:02 +0100

    Setup barebones socketio app

--- server.js
+++ server.js
@@ -3,29 +3,13 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-var p2p = require('socket.io-p2p-server').Server;
-io.use(p2p);
-
 io.on('connection', function (socket) {
   socket.on('peer-msg', function (data) {
-    console.log('Message from peer: %s', data)
-    socket.broadcast.emit('peer-msg', data)
-  });
-
-  socket.on('peer-file', function (data) {
-    console.log('File from peer: %s', data)
-    socket.broadcast.emit('peer-file', data)
-  });
-
-  socket.on('go-private', function (data) {
-    socket.broadcast.emit('go-private', data)
+    console.log('Message from peer: %s', data);
+    socket.broadcast.emit('peer-msg', data);
   });
 });
 
 app.use(express.static('public'));
-
-/*app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/client/index.html');
-});*/
  
 io.listen(app.listen(3000));
