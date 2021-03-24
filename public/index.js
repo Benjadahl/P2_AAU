ca1dacaabd2baed79452e521e8d56bcf4d2ad965
benjadahl <benjadahl@gmail.com>
2021-03-24 13:50:42 +0100

    Added chat log feature

--- public/index.js
+++ public/index.js
@@ -4,6 +4,16 @@ function sendMsg (data) {
   socket.emit('msg', data);
 }
 
-socket.on('peer-msg', data => {
+function printMsg (data) {
   console.log('Message: ' + data);
+}
+
+socket.on('peer-msg', data => {
+  printMsg(data);
+});
+
+socket.on('chatLog', chatLog => {
+  chatLog.forEach(msg => {
+    printMsg(msg);
+  });
 });
--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+let chatLog = new Array();
+
 io.on('connection', socket => {
   socket.on('msg', data => {
     console.log('Message: ' + data);
+    chatLog.push(data);
     socket.broadcast.emit('peer-msg', data);
   });
+
+  socket.emit('chatLog', chatLog);
 });
 
 app.use(express.static('public'));
