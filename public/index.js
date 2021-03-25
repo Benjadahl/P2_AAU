d5794c70f81afbaeaf1537505b9f00124f608480
Casper Andersen <casper@manjaro.home>
2021-03-25 14:31:03 +0100

    fixed ui when sending msg, need to look at there pos next

--- public/index.js
+++ public/index.js
@@ -1,11 +1,28 @@
 const socket = io();
 
-function sendMsg (data) {
+document.getElementById("sendMsg").addEventListener("click", () => {
+  
+    sendMsg(document.getElementById("exampleDataList").value);
+    document.getElementById("exampleDataList").value = "";
+});
+function sendMsg(data) {
   socket.emit('msg', data);
+  printMsg(data, true);
 }
 
-function printMsg (data) {
+function printMsg(data, me) {
   console.log('Message: ' + data);
+  let newtxt = document.createElement("H6");
+  newtxt.innerText = data;
+  /*newtxt.style.position = "absolute"
+  if(me === true){
+    newtxt.style.right = 0;
+  }
+  else{
+    newtxt.style.right = 0;
+  }*/
+  
+  document.getElementById("sendt").appendChild(newtxt);
 }
 
 socket.on('peer-msg', data => {
