3e50fa9bc253be8b4baa58844e8e5a41d10b1915
Casper Andersen <casper@manjaro.home>
2021-03-26 12:23:27 +0100

    made the postion of msg change based on the wheter they are sendt or recived

--- public/index.js
+++ public/index.js
@@ -6,23 +6,24 @@ document.getElementById("sendMsg").addEventListener("click", () => {
     document.getElementById("exampleDataList").value = "";
 });
 function sendMsg(data) {
-  socket.emit('msg', data);
+  socket.emit('msg', data, false);
   printMsg(data, true);
 }
 
 function printMsg(data, me) {
   console.log('Message: ' + data);
   let newtxt = document.createElement("H6");
+  //let pos = document.createElement("DIV");
   newtxt.innerText = data;
-  /*newtxt.style.position = "absolute"
+  //newtxt.style.position = "relative"
   if(me === true){
-    newtxt.style.right = 0;
+    document.getElementById("sendt").appendChild(newtxt);
   }
   else{
-    newtxt.style.right = 0;
-  }*/
+    document.getElementById("recived").appendChild(newtxt);
+  }
   
-  document.getElementById("sendt").appendChild(newtxt);
+  
 }
 
 socket.on('peer-msg', data => {
