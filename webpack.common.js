e6dd97ae426cc552b9201d857be859c2d3757b80
benjadahl <benjadahl@gmail.com>
2021-04-06 10:44:13 +0200

    Added build scripts

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import CopyPlugin from 'copy-webpack-plugin';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default {
+  entry: {
+    "index": "./src/index.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "imgs"
+          }
+        }
+      }
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        "src/index.html"
+      ],
+    }),
+  ]
+};
