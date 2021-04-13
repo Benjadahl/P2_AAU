0a9792352a51c16e5fdb20a10faa8de07239de57
benjadahl <benjadahl@gmail.com>
2021-04-13 13:59:14 +0200

    Add line breaks

--- torben/server.js
+++ torben/server.js
@@ -7,10 +7,12 @@ export default function setupTorbenServer (io) {
   io.on('connection', socket => {
     socket.on('getTorbenID', peerID => {
       const torbenID = crypto.randomBytes(16).toString('hex');
+
       connections[torbenID] = {
         peerID: peerID,
         socket: socket
       }
+      
       socket.emit('torbenID', torbenID);
       console.log(connections);
     });
