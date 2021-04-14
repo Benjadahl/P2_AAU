a297c9a45d07f1fa75a79654c074b89e65049843
Casper Andersen <casper11andersen@gmail.com>
2021-04-14 14:41:17 +0200

    made it so that there cant be issued a repeat of a torbenID

--- torben/server/getTorbenID.js
+++ torben/server/getTorbenID.js
@@ -1,13 +1,19 @@
 import crypto from 'crypto';
 
-export default function getTorbenID (socket, peerID, connections) {
-  const torbenID = crypto.randomBytes(16).toString('hex');
+export default function getTorbenID(socket, peerID, connections) {
+  let torbenID;
+
+  while (connections[torbenID] != undefined || torbenID == undefined) {
+    torbenID = crypto.randomBytes(16).toString('hex');
+    console.log("here")
+  };
 
   connections[torbenID] = {
     peerID: peerID,
     socket: socket
   }
-  
+
+
   socket.emit('torbenID', torbenID);
   return torbenID;
 }
