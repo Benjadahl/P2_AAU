c47d4dd18588ad99307ffebdb2b97677a6c74283
benjadahl <benjadahl@gmail.com>
2021-04-19 11:44:18 +0200

    Corrected format of test traces

--- helperScripts/tracerouteTest.js
+++ helperScripts/tracerouteTest.js
@@ -2,13 +2,19 @@ import Traceroute from 'nodejs-traceroute';
 import fs from 'fs';
 
 const tracer = new Traceroute();
-let hops = [];
+let hops = {};
   
+let lastHop = hops;
+
 tracer.on('hop', hop => {
-  hops.push(hop);
+  if (hop.ip !== "*") {
+    lastHop[hop.ip] = {};
+    lastHop = lastHop[hop.ip];
+    console.log(hops);
+  }
 }).on('close', (code) => {
   fs.writeFile('hops.json', JSON.stringify(hops), 'utf8', () => {
     console.log("Wrote file");
   });
-}).trace("212.237.248.11");
+}).trace("85.203.245.236");
 
