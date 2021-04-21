0a105053822087917e2344dfb24dbb31f93fd40a
benjadahl <benjadahl@gmail.com>
2021-04-21 12:22:52 +0200

    Turned test traces into proper base traces

--- helperScripts/tracerouteTest.js
+++ helperScripts/tracerouteTest.js
@@ -1,20 +1,27 @@
 import Traceroute from 'nodejs-traceroute';
 import fs from 'fs';
+import getBase from '../torben/server/getBase.js';
 
 const tracer = new Traceroute();
 let hops = {};
   
 let lastHop = hops;
+let lastBase;
 
 tracer.on('hop', hop => {
-  if (hop.ip !== "*") {
-    lastHop[hop.ip] = {};
-    lastHop = lastHop[hop.ip];
-    console.log(hops);
+  const ip = hop.ip;
+  if (ip !== "*") {
+    console.log(ip);
+    const base = getBase(ip);
+    if (lastBase !== base) {
+      lastBase = base;
+      lastHop[base] = {};
+      lastHop = lastHop[base];
+    }
   }
 }).on('close', (code) => {
   fs.writeFile('hops.json', JSON.stringify(hops), 'utf8', () => {
     console.log("Wrote file");
   });
-}).trace("85.203.245.236");
+}).trace("85.191.209.89");
 
