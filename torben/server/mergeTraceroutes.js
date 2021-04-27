b044496da41bbef87b82ac1d33ec90763d291cc6
benjadahl <benjadahl@gmail.com>
2021-04-27 12:37:20 +0200

    Added tree lib boilerplate

--- torben/server/mergeTraceroutes.js
+++ torben/server/mergeTraceroutes.js
@@ -1,71 +1,13 @@
-export default function mergeTraceroutes(trX, trY) {
-  getSubtree(trX, "69").then(r => {
-    console.log(JSON.stringify(r, null, 2));
-  });
+import TreeModel from 'tree-model';
+
+export default function mergeTraceroutes (trX, trY) {
+  let tree1 = new TreeModel();
+  let root = tree1.parse({name: 'a', children: [{name: 'b'}]})
+  let root2 = tree1.parse({name: 'a', children: [{name: 'c'}]})
+  root.addChild(root2)
   
-  /*let promiseListX = [];
-  let promiseListY = [];
-  getIPRecursive(trX, Object.keys(trX)[0], promiseListX);
-  getIPRecursive(trY, Object.keys(trY)[0], promiseListY);
-  Promise.all(promiseListX).then(x => {
-    Promise.all(promiseListY).then(y => {
-      console.log(x);
-      console.log(y);
-
-      let match;
-
-      x.forEach(xIp => {
-        y.forEach(yIp => {
-          if (xIp.ip === yIp.ip && match == null) {
-            match = { ip: xIp.ip, idX: xIp.id, idY: yIp.id };
-          }
-        });
-      });
-
-      console.log(match);
-    });
-  });*/
-
-  //return trX;
-}
-
-function getIPRecursive(obj, ip, promiseList) {
-  return new Promise((resolve, reject) => {
-    Object.keys(obj).forEach(child => {
-      if (child !== "torbenClients" && child !== "id") {
-        promiseList.push(getIPRecursive(obj[child], child, promiseList));
-      }
-      resolve({ ip: ip, id: obj.id });
-    });
-  });
-}
-
-function insertSubtree(obj, id, subtree) {
-
-}
-
-function getSubtree(obj, id) {
-  return new Promise((resolve, reject) => {
-    if (obj.id === id) {
-      resolve(obj);
-    } else {
-      let childTraverses = [];
-
-      Object.keys(obj).forEach(child => {
-        if (child !== "torbenClients" && child !== "id") {
-          childTraverses.push(getSubtree(obj[child], id));
-        } else if (child === "torbenClients") {
-          resolve(null);
-        }
-      });
-
-      Promise.all(childTraverses).then(c => {
-        c.forEach(result => {
-          if (result != null) {
-            resolve(result);
-          }
-        });
-      });
-    }
+  
+  root.walk([], node => {
+    console.log(node.model.name);
   });
 }
