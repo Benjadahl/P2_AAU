929e9361c4f4b5db166fd296bc1a0282537574eb
benjadahl <benjadahl@gmail.com>
2021-04-27 13:55:44 +0200

    Made mergeTraceroute work

--- torben/server/mergeTraceroutes.js
+++ torben/server/mergeTraceroutes.js
@@ -1,6 +1,15 @@
 export default function mergeTraceroutes (trX, trY) {
-  
-  trX.walk([], node => {
-    console.log(node.model.ip);
+  let yMatch;
+
+  let xMatch = trX.first({strategy: 'post'}, nodeX => {
+    return yMatch = trY.first({strategy: 'post'}, nodeY => {
+      if (nodeX.model.ip === nodeY.model.ip) {
+        return true;
+      }
+    });
   });
+
+  xMatch.addChild(yMatch.children[0]);
+
+  console.log(JSON.stringify(trX.model, null, 2));
 }
