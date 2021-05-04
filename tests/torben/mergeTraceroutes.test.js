dd366f64ccdfc5ade5cfcab6f2292fa40a9f4e39
benjadahl <benjadahl@gmail.com>
2021-05-04 09:27:40 +0200

    Added snapshot tests

--- tests/torben/mergeTraceroutes.test.js
+++ tests/torben/mergeTraceroutes.test.js
@@ -2,21 +2,24 @@ import test from 'ava';
 import mergeTraceroutes from "../../torben/server/mergeTraceroutes.js";
 import fs from 'fs';
 import TreeModel from 'tree-model';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const tree = new TreeModel();
 
-const casperTrace = tree.parse(JSON.parse(fs.readFileSync('./traces/casper.json')));
-const lukasTrace = tree.parse(JSON.parse(fs.readFileSync('./traces/lukas.json')));
-const avaTrace = tree.parse(JSON.parse(fs.readFileSync('./traces/ava.json')));
-const jonasTrace = tree.parse(JSON.parse(fs.readFileSync('./traces/jonas.json')));
-const marcusTrace = tree.parse(JSON.parse(fs.readFileSync('./traces/marcus.json')));
+const casperTrace = tree.parse(JSON.parse(fs.readFileSync(path.resolve(__dirname, './traces/casper.json'))));
+const lukasTrace = tree.parse(JSON.parse(fs.readFileSync(path.resolve(__dirname, './traces/lukas.json'))));
+const avaTrace = tree.parse(JSON.parse(fs.readFileSync(path.resolve(__dirname, './traces/ava.json'))));
+const jonasTrace = tree.parse(JSON.parse(fs.readFileSync(path.resolve(__dirname, './traces/jonas.json'))));
+const marcusTrace = tree.parse(JSON.parse(fs.readFileSync(path.resolve(__dirname, './traces/marcus.json'))));
 
 test('Merge two traces together', t => {
   let map = mergeTraceroutes(casperTrace, lukasTrace);
   map = mergeTraceroutes(map, avaTrace);
   map = mergeTraceroutes(map, jonasTrace);
   map = mergeTraceroutes(map, marcusTrace);
-  console.log(JSON.stringify(map.model, null, 2));
-  fs.writeFile('hops.json', JSON.stringify(map.model, null, 2), 'utf8', () => {});
-  t.is(true, true);
+  t.snapshot(map);
 });
