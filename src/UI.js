d38ae9f69c1b408f7666f7b723d42da2f9d93132
Casper Andersen <casper11andersen@gmail.com>
2021-05-04 09:34:51 +0200

    minor changes

--- src/UI.js
+++ src/UI.js
@@ -5,25 +5,25 @@ function clearMembersList() {
   membersList.innerHTML = "";
 }
 
-function updatesMemberList(usernames) { 
- 
-    clearMembersList();
-    for (let i = 0; i < usernames.length; i++) {
-      let newRow = membersList.insertRow(0);
-      let newCell1 = newRow.insertCell(0);
-      let newCell2 = newRow.insertCell(1);
-      newCell1.innerHTML = usernames[i];
-      console.log(usernames[i]);
-      newCell2.innerHTML = "Online";
-    }
-  
+function updatesMemberList(usernames) {
+
+  clearMembersList();
+  for (let i = 0; i < usernames.length; i++) {
+    let newRow = membersList.insertRow(0);
+    let newCell1 = newRow.insertCell(0);
+    let newCell2 = newRow.insertCell(1);
+    newCell1.innerHTML = usernames[i];
+    console.log(usernames[i]);
+    newCell2.innerHTML = "Online";
+  }
+
 }
 
-function resizer(){
+function resizer() {
   let ratio;
-  ratio=1-((1/window.innerHeight)*75);
+  ratio = 1 - ((1 / window.innerHeight) * 75);
   let height = window.innerHeight * ratio;
   document.getElementById("sendt").style.setProperty("max-height", height.toString() + "px");
 }
 
-export { clearMembersList, updatesMemberList, resizer};
+export { clearMembersList, updatesMemberList, resizer };
