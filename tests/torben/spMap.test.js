031cb197a1c2294bb72b5f41f3d55bb6fdc39691
benjadahl <benjadahl@gmail.com>
2021-05-13 10:53:53 +0200

    Added weight map function

---
+++ tests/torben/spMap.test.js
@@ -0,0 +1,17 @@
+import test from 'ava';
+import weightMap from '../../torben/client/weightMap.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import TreeModel from 'tree-model';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const tree = new TreeModel();
+
+const testTree = tree.parse(JSON.parse(fs.readFileSync(path.resolve(__dirname, './traces/all.json'))));
+
+test('Run weightMap', t => {
+  t.snapshot(weightMap(testTree));
+});
---
+++ torben/client/weightMap.js
@@ -0,0 +1,26 @@
+/*
+  weight map - using dijkstras algorithm from dijkstra.js
+  in: traceroute map
+  out: dictionary for path weights between torben IDs
+*/
+
+import dijkstra from './dijkstra.js';
+
+export default function weightMap (map) {
+  let wMap = {};
+  let clients = [];
+
+  map.all().forEach(node => {
+    if (node.model.torbenIDs != null) {
+      node.model.torbenIDs.forEach(ID => {
+        clients.push(ID);
+      });
+    }
+  });
+
+  clients.forEach(ID => {
+    wMap[ID] = dijkstra(map, ID);
+  });
+
+  return wMap;
+}
