cbb4a3e45f133f1d64c06f463d8b7013cfbf4af8
benjadahl <benjadahl@gmail.com>
2021-05-13 13:43:52 +0200

    Made a very crude emitPath implementation

---
+++ tests/torben/getEmitPath.test.js
@@ -0,0 +1,20 @@
+import test from 'ava';
+import getEmitPath from '../../torben/client/getEmitPath.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import TreeModel from 'tree-model';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const tree = new TreeModel();
+
+const testTree = tree.parse(JSON.parse(fs.readFileSync(path.resolve(__dirname, './traces/all.json'))));
+
+test('GetEmitPath', t => {
+  return getEmitPath(testTree, "Ava", ["Casper", "Jonas", "Lukas", "Marcus"]).then(res => {
+    console.log(JSON.stringify(res, null, 2));
+    t.snapshot(res);
+  });
+});
---
+++ torben/client/getEmitPath.js
@@ -0,0 +1,67 @@
+/*
+  get emit path - calculate the path to send a message via, using the weight map
+  in: trace route map, recievers of the message (if empty send to all)
+  out: an ideal path
+
+       {
+         torbenID1:
+         {
+           torbenID2: {
+             torbenID3,
+             torbenID4
+           },
+           torbenID5: {
+             torbenID6
+           }
+         }
+       }
+*/
+
+import weightMap from './weightMap.js';
+
+export default function getEmitPath (map, sender, recievers) {
+  return new Promise ((resolve, reject) => {
+    const wMap = weightMap(map);
+  
+    //console.log(wMap[sender]);
+
+    let promises = [];
+    let paths = [];
+  
+    if (recievers.length > 0) {
+      recievers.forEach(reciever => {
+        const newRecievers = recievers.filter(r => r !== reciever);
+        const newSender = reciever;
+        let recur = getEmitPath(map, newSender, newRecievers).then(recursion => {
+          console.log(wMap[sender][reciever]);
+          console.log(recursion);
+          let path = {};
+          path[sender] = {};
+          path[sender][reciever] = recursion.path;
+
+          paths.push({
+            weight: wMap[sender][reciever] + recursion.weight,
+            path: path
+          });
+        });
+        promises.push(recur);
+      });
+  
+      Promise.all(promises).then(() => {
+        console.log(paths);
+
+        let minOption = null;
+
+        for (let option of paths) {
+          if (minOption == null || minOption.weight > option.weight ) {
+            minOption = option;
+          }
+        }
+
+        resolve(minOption);
+      });
+    } else {
+      resolve({weight: 0, path: {}});
+    }
+  });
+}
