b8fafdb67a8be7008e2c69cfe631021931af325d
benjadahl <benjadahl@gmail.com>
2021-05-17 12:28:46 +0200

    Added boilerplate for arrayMutation function

---
+++ tests/torben/arrayMutation.test.js
@@ -0,0 +1,3 @@
+import test from 'ava';
+import arrayMutation from '../../torben/client/arrayMutation.js';
+
---
+++ torben/client/arrayMutation.js
@@ -0,0 +1,11 @@
+/*
+  generate all possible mutations of an array, containing all of the items of the original
+  array
+
+  in: array
+  out: array containing all mutations of the array
+*/
+
+export default function arrayMutations (arrayIn) {
+
+}
