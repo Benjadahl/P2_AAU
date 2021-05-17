dc811fa16c29c1cd30e99c0b7bed2e7dc78f79d4
benjadahl <benjadahl@gmail.com>
2021-05-17 13:37:53 +0200

    Added getCombinations and uniqueSummingSubset functions

--- tests/torben/arrayMutation.test.js
+++ tests/torben/arrayMutation.test.js
@@ -1,3 +1,21 @@
 import test from 'ava';
 import arrayMutation from '../../torben/client/arrayMutation.js';
+import {getCombinations} from '../../torben/client/arrayMutation.js';
+import {uniqueSummingSubsets} from '../../torben/client/arrayMutation.js';
+
+/*test('GetCombinations 3 elements', t => {
+  t.snapshot(getCombinations(["ID1", "ID2", "ID3"]));
+});
+
+test('GetCombinations 4 elements', t => {
+  t.snapshot(getCombinations(["ID1", "ID2", "ID3", "ID4"]));
+});*/
+
+test('GetCombinations 5 elements', t => {
+  t.snapshot(getCombinations(["ID1", "ID2", "ID3", "ID4", "ID5", "ID6"]));
+});
+
+test('uniqueSummingSubsets 4', t => {
+  t.snapshot(uniqueSummingSubsets(5));
+});
 
--- torben/client/arrayMutation.js
+++ torben/client/arrayMutation.js
@@ -6,6 +6,95 @@
   out: array containing all mutations of the array
 */
 
-export default function arrayMutations (arrayIn) {
+export default function arrayMutations(arrayIn) {
+  const combinations = getCombinations(arrayIn);
 
+
+}
+
+export function getCombinations(valuesArray) {
+  var combi = {};
+  var temp = [];
+  var slent = Math.pow(2, valuesArray.length);
+
+  for (var i = 0; i < slent; i++) {
+    temp = [];
+    for (var j = 0; j < valuesArray.length; j++) {
+      if ((i & Math.pow(2, j))) {
+        temp.push(valuesArray[j]);
+      }
+    }
+    if (temp.length > 0) {
+      if (combi[temp.length] == null) {
+        combi[temp.length] = [];
+      }
+      combi[temp.length].push(temp);
+    }
+  }
+
+  //console.log(combi);
+
+  return combi;
+}
+
+export function uniqueSummingSubsets (target) {
+  const allSubsets = getSummingSubsets(target);
+  let allSums = [];
+  let toReturn = [];
+
+  allSubsets.forEach(subset => {
+    let sum = [];
+
+    subset.forEach(number => {
+      if (sum[number] != null) {
+        sum[number]++;
+      } else {
+        sum[number] = 0;
+      }
+    });
+
+    let equal;
+    for (let i = 0; i < allSums.length && !equal; i++) {
+      equal = true;
+
+      for (let j = 0; j < target; j++) {
+        if (sum[j] !== allSums[i][j]) {
+          equal = false;
+        }
+      }
+    }
+
+    if (!equal) {
+      allSums.push(sum);
+      toReturn.push(subset);
+    }
+
+  });
+
+  return toReturn;
+}
+
+export function getSummingSubsets(target) {
+  let set_arr = [];
+
+  for (let i = target; i > 0; i--) {
+    set_arr.push(i);
+  }
+
+  let finish = [];
+  let working = [[]];
+  while (working.length) {
+    let next_work = [];
+    for (let i = 0; i < working.length; i++) {
+      for (let j = 0; j < set_arr.length; j++) {
+        let subset = working[i].concat([set_arr[j]]);
+        let sum = subset.reduce((a, b) => a + b, 0);
+        if (sum <= target) {
+          (sum == target ? finish : next_work).push(subset);
+        }
+      }
+    }
+    working = next_work
+  }
+  return finish;
 }
