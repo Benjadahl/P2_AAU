d640381ec1109f8091ebf98ca2eada0f0be4a8c6
benjadahl <benjadahl@gmail.com>
2021-05-21 10:04:32 +0200

    Added offline status for clients

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const http = createServer(app);
 const io = new Server(http, {});
 import torben from "./torben/server.js";
 
-let listOfMembers = [];
+let listOfMembers = {};
 let chatLog = [];
 
 function logMsg(msg) {
@@ -20,18 +20,51 @@ io.on('connection', socket => {
     /* Function is called when a new peer connects */
     socket.on('userLogin', data => {
         socket.emit('chatLog', chatLog);
-        //If username is not already taken
-        if (!listOfMembers.includes(data.username)) {
-            listOfMembers.push(data.username);
+
+        for (let member in listOfMembers) {
+            const details = listOfMembers[member];
+
+            if (details.username === data.username) {
+                if (details.online) {
+                    throw 'Duplicate usernames online';
+                } else {
+                    delete listOfMembers[member];
+                }
+            }
         }
+
+        listOfMembers[socket.id] = {
+            username: data.username,
+            online: true
+        };
+
         /*emits the usernames to all users so that they can be shown in the interface*/
-        io.sockets.emit('login', listOfMembers);
+        sendMemberList(listOfMembers, io);
     });
 
     socket.on('logMsg', msg => {
         logMsg(msg);
     });
+
+    socket.on('disconnect', () => {
+        if (listOfMembers[socket.id] != null) {
+            listOfMembers[socket.id].online = false;
+            sendMemberList(listOfMembers, io);
+        }
+    });
 });
+
+/* Strip memberlist object of socket ID's to not 
+expose them to the clients */
+function sendMemberList(memberList, io) {
+    let toSend = [];
+
+    for (let member in memberList) {
+        toSend.push(memberList[member]);
+    }
+
+    io.sockets.emit('login', toSend);
+}
 
 /* Serve the dist folder via Express */
 app.use(express.static('dist'));
