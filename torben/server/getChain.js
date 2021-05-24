026a21ee3a54ed91626c6a0266ef1665c8f1cdf2
benjadahl <benjadahl@gmail.com>
2021-05-24 11:13:46 +0200

    Made getChain chose the smallest weight

--- torben/server/getChain.js
+++ torben/server/getChain.js
@@ -1,21 +1,27 @@
 import getClientsInTree from "./getClientsInTree.js";
 import getEmitPath from "./getEmitPath.js";
 import pathToArray from "./pathToArray.js";
+import weightSort from "./weightSort.js";
 
 export default function getChain (map) {
   return new Promise ((resolve, reject) => {
     const all = getClientsInTree(map);
-    const sender = all[0];
-    const recievers = all.slice(1);
+    let pathCalculations = [];
+    
+    for (let i = 0; i < all.length; i++) {
+      const sender = all[i];
+      const recievers = all.filter(ID => ID !== sender);
+      
+      pathCalculations.push(getEmitPath(map, sender, recievers));
+    }
 
-    getEmitPath(map, sender, recievers).then(emitPath => {
-      //Indsæt kode der finder den laveste vægt
-  
-      let chain = [sender];
-  
+    Promise.all(pathCalculations).then(emitPaths => {
+      emitPaths.sort(weightSort);
+      console.log(emitPaths);
+      const emitPath = emitPaths[0];
+      let chain = [emitPath.sender];
+      
       resolve(chain.concat(pathToArray(emitPath.path)));
     });
-
-
   });
 }
